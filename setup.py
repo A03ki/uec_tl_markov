0eb51bd86f8470e506549896ca168a9b7e27934d
Aki <a03ki04@gmail.com>
2020-09-12 14:28:33 +0900

    long_descriptionとlong_description_content_typeを追加 #1

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
+
 from setuptools import find_packages, setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md")) as f:
+    readme = f.read()
 
 
 packages = find_packages(exclude=["workspace"])
@@ -7,6 +15,8 @@ setup_args = {
     "name": "uectl",
     "version": "0.1.0",
     "description": "@uec_tlに使用するマルコフ連鎖による文章生成とテキスト処理のためのパッケージ",
+    "long_description": readme,
+    "long_description_content_type": "text/markdown",
     "license": "MIT License",
     "author": "Aki(@puman03)",
     "author_email": "a03ki04@gmail.com",
